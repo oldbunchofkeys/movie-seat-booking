3c7c5ecddaf8b34b6aad42f8e40877cc0a358b93
Pete Thompson <peter.thompson@roswellstudios.com>
2023-07-13 12:42:41 -0400

    refactor: organize code a bit

--- index.js
+++ index.js
@@ -21,70 +21,74 @@ const seats = [
     booked: false
   }
 ]
-// helpers for data to/from localstorage
+// helper for data to localstorage
 function jsonSeatsToLocalStorage(arr) {
   let jsonSeats = JSON.stringify(arr);
   localStorage.setItem('jsonSeats', jsonSeats);
 }
-const jsonLocalSeats = localStorage.getItem('jsonSeats');
-let parsedLocalSeats = JSON.parse(jsonLocalSeats);
-console.log(parsedLocalSeats);
+
 // on page load, read localstorage values and add true values for 'booked' as DOM classes
 document.addEventListener('DOMContentLoaded', ()=> { 
-  if (parsedLocalSeats !== null) {
-    parsedLocalSeats.map((seat, index) => {
-      if (seat.booked == true) {
-        document.querySelector(`#seat${index + 1}`).classList.add('booked');
+  const jsonLocalSeats = localStorage.getItem('jsonSeats');
+  let parsedLocalSeats = JSON.parse(jsonLocalSeats);
+  const domSeats = [
+    document.querySelector("#seat1"), 
+    document.querySelector("#seat2"), 
+    document.querySelector("#seat3"), 
+    document.querySelector("#seat4"), 
+    document.querySelector("#seat5")
+  ];
+
+  //data mapping
+  (function () {
+    if (parsedLocalSeats !== null) {
+      parsedLocalSeats.map((seat, index) => {
+        if (seat.booked == true) {
+          document.querySelector(`#seat${index + 1}`).classList.add('booked');
+        }
+      });
+    }
+    seats.map((seat, index) => {
+      if (seat.occupied == true) {
+        document.querySelector(`#seat${index + 1}`).classList.add('occupied');
       }
     });
-  }
-});
-// data mapping
-seats.map((seat, index) => {
-  if (seat.occupied == true) {
-    document.querySelector(`#seat${index + 1}`).classList.add('occupied');
-  }
-});
-
-// event listeners
-const domSeats = [
-  document.querySelector("#seat1"), 
-  document.querySelector("#seat2"), 
-  document.querySelector("#seat3"), 
-  document.querySelector("#seat4"), 
-  document.querySelector("#seat5")
-]
-domSeats.forEach((domSeat, index) => {
-  domSeat.addEventListener('click', () => {
-    if (!domSeat.classList.contains('occupied')) {
-      if (domSeat.classList.contains('booked')) {
-        domSeat.classList.remove('booked');
-      } else {
-        domSeat.classList.add('booked');
+  })();
+  
+  // event listeners
+  domSeats.forEach((domSeat, index) => {
+    domSeat.addEventListener('click', () => {
+      if (!domSeat.classList.contains('occupied')) {
+        if (domSeat.classList.contains('booked')) {
+          domSeat.classList.remove('booked');
+        } else {
+          domSeat.classList.add('booked');
+        }
       }
-    }
-    if (parsedLocalSeats === null) {
-      seats.forEach((seat, index2) => {
-        if (index == index2) {
-          if (domSeat.classList.contains('booked')) {
-            seat.booked = true
-          } else {
-            seat.booked = false
+      if (parsedLocalSeats === null) {
+        seats.forEach((seat, index2) => {
+          if (index == index2) {
+            if (domSeat.classList.contains('booked')) {
+              seat.booked = true
+            } else {
+              seat.booked = false
+            }
           }
-        }
-      });
-      jsonSeatsToLocalStorage(seats);
-    } else {
-      parsedLocalSeats.forEach((seat, index2) => {
-        if (index == index2) {
-          if (domSeat.classList.contains('booked')) {
-            seat.booked = true
-          } else {
-            seat.booked = false
+        });
+        jsonSeatsToLocalStorage(seats);
+      } else {
+        parsedLocalSeats.forEach((seat, index2) => {
+          if (index == index2) {
+            if (domSeat.classList.contains('booked')) {
+              seat.booked = true
+            } else {
+              seat.booked = false
+            }
           }
-        }
-      });
-      jsonSeatsToLocalStorage(parsedLocalSeats);
-    }
+        });
+        jsonSeatsToLocalStorage(parsedLocalSeats);
+      }
+    });
   });
 });
+
